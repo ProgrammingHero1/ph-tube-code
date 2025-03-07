2abfd6f86b9b1ec834de11d52a135a2fa9a91860
ferdouszihad <f.zihad333@gmail.com>
2025-03-08 00:20:12 +0600

    category button display completed

--- script/index.js
+++ script/index.js
@@ -1,14 +1,37 @@
 console.log("index is connected");
 
 function loadCategories() {
-  // fetch the data
-
+  // 1- fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    //2 - convert promise to json
     .then((res) => res.json())
+    //3 - send data to display
     .then((data) => displayCategories(data.categories));
 }
+
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
+
 function displayCategories(categories) {
-  console.log(categories);
+  // get the container
+  const categoryContainer = document.getElementById("category-container");
+
+  // Loop operation on Array of object
+  for (let cat of categories) {
+    console.log(cat);
+
+    // create Element
+    const categoryDiv = document.createElement("div");
+
+    categoryDiv.innerHTML = `
+    <button class="btn btn-sm hover:bg-[#FF1F3D]  hover:text-white">${cat.category}</button>
+    `;
+
+    // Append the Element
+    categoryContainer.append(categoryDiv);
+  }
 }
 
 loadCategories();
