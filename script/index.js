e0a1f2f4ad5ec903865bbc4e2e3fbe6cae294a6c
ferdouszihad <f.zihad333@gmail.com>
2025-03-08 02:43:17 +0600

    5- Design video card & Display dynamic data

--- script/index.js
+++ script/index.js
@@ -67,21 +67,42 @@ const displayVideos = (videos) => {
     const videoCard = document.createElement("div");
 
     videoCard.innerHTML = `
+     <div class="card bg-base-100">
+        <figure class="relative">
+          <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="Shoes" />
+          <span
+            class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2"
+            >3hrs 56 min ago</span
+          >
+        </figure>
 
-    <div class="card bg-base-100  shadow-sm">
-  <figure>
-    <img
-      src="${video.thumbnail}"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+        <div class="flex gap-3 px-0 py-5">
+          <div class="profile">
+            <div class="avatar">
+              <div
+                class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2"
+              >
+                <img
+                  src="${video.authors[0].profile_picture}"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="intro">
+            <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+            <p class="text-sm text-gray-400 flex gap-1">
+             ${video.authors[0].profile_name}
+              <img
+                class="w-5 h-5"
+                src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
+                alt=""
+              />
+            </p>
+            <p class="text-sm text-gray-400">${video.others.views} views</p>
+          </div>
+        </div>
+      </div>
     
     `;
     //append
