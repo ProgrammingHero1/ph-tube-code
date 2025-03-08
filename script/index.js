d5f44a5fa20c80588da9d94ff26b1a7e75d02418
ferdouszihad <f.zihad333@gmail.com>
2025-03-08 22:36:16 +0600

    6- load videos by category

--- script/index.js
+++ script/index.js
@@ -15,6 +15,15 @@ function loadVideos() {
     .then((data) => displayVideos(data.videos));
 }
 
+const loadCategoryVideos = (id) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+  console.log(url);
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.category));
+};
+
 // {
 //     "category_id": "1001",
 //     "category": "Music"
@@ -31,7 +40,7 @@ function displayCategories(categories) {
     const categoryDiv = document.createElement("div");
 
     categoryDiv.innerHTML = `
-    <button class="btn btn-sm hover:bg-[#FF1F3D]  hover:text-white">${cat.category}</button>
+    <button  onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D]  hover:text-white">${cat.category}</button>
     `;
 
     // Append the Element
@@ -60,6 +69,8 @@ function displayCategories(categories) {
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("video-container");
+
+  videoContainer.innerHTML = "";
 
   videos.forEach((video) => {
     console.log(video);
@@ -111,4 +122,3 @@ const displayVideos = (videos) => {
 };
 
 loadCategories();
-loadVideos();
