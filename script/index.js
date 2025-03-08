9c13c94014c731e2a0716181fa288ccacc4ddf7e
ferdouszihad <f.zihad333@gmail.com>
2025-03-09 02:38:13 +0600

    9+10 - Modal + handle conditional data + search

--- script/index.js
+++ script/index.js
@@ -15,8 +15,10 @@ function loadCategories() {
     .then((data) => displayCategories(data.categories));
 }
 
-function loadVideos() {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+function loadVideos(searchText = "") {
+  fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`
+  )
     .then((response) => response.json())
     .then((data) => {
       removeActiveClass();
@@ -40,6 +42,36 @@ const loadCategoryVideos = (id) => {
 
       displayVideos(data.category);
     });
+};
+const loadVideoDetails = (videoId) => {
+  console.log(videoId);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideoDetails(data.video));
+};
+
+const displayVideoDetails = (video) => {
+  console.log(video);
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details-container");
+
+  detailsContainer.innerHTML = `
+   <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      
+    </div>
+  </div>
+</div>
+  `;
 };
 
 // {
@@ -112,7 +144,9 @@ const displayVideos = (videos) => {
     videoCard.innerHTML = `
      <div class="card bg-base-100">
         <figure class="relative">
-          <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="Shoes" />
+          <img class="w-full h-[150px] object-cover" src="${
+            video.thumbnail
+          }" alt="Shoes" />
           <span
             class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2"
             >3hrs 56 min ago</span
@@ -136,15 +170,23 @@ const displayVideos = (videos) => {
             <h2 class="text-sm font-semibold">Midnight Serenade</h2>
             <p class="text-sm text-gray-400 flex gap-1">
              ${video.authors[0].profile_name}
-              <img
+              ${
+                video.authors[0].verified == true
+                  ? `<img
                 class="w-5 h-5"
                 src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
                 alt=""
-              />
+              />`
+                  : ``
+              }
             </p>
             <p class="text-sm text-gray-400">${video.others.views} views</p>
           </div>
+
         </div>
+        <button onclick=loadVideoDetails('${
+          video.video_id
+        }') class="btn btn-block">Show Details</button>
       </div>
     
     `;
@@ -153,4 +195,8 @@ const displayVideos = (videos) => {
   });
 };
 
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+  const input = e.target.value;
+  loadVideos(input);
+});
 loadCategories();
